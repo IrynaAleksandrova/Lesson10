c11050383a6658c991a43dbe1ee273ce1c591b10
Iryna Lytkina <lytkina.iryna@gmail.com>
2022-10-21 12:57:37 +0300

    one task

---
+++ src/task1/Main.java
@@ -0,0 +1,30 @@
+package task1;
+
+public class Main {
+//    Написать программу со следующим функционалом:
+//    На вход передать строку (будем считать, что это номер документа).
+//    Номер документа имеет формат xxxx-yyy-xxxx-yyy-xyxy, где x — это число, а y — это буква.
+//    - Вывести на экран в одну строку два первых блока по 4 цифры.
+//    - Вывести на экран номер документа, но блоки из трех букв заменить на *** (каждая буква заменятся на *).
+//    - Вывести на экран только одни буквы из номера документа в формате yyy/yyy/y/y в нижнем регистре.
+//    - Вывести на экран буквы из номера документа в формате "Letters:yyy/yyy/y/y" в верхнем регистре
+//    (реализовать с помощью класса StringBuilder).
+//    - Проверить содержит ли номер документа последовательность abc и вывети сообщение содержит или нет
+//    (причем, abc и ABC считается одинаковой последовательностью).
+//    - Проверить начинается ли номер документа с последовательности 555.
+//    - Проверить заканчивается ли номер документа на последовательность 1a2b.
+//    Все эти методы реализовать в отдельном классе в статических методах,
+//    которые на вход (входным параметром) будут принимать вводимую на вход программы строку.
+
+    public static void main(String[] args) {
+        String document = "1234-ABC-5678-cde-9q8w";
+
+        WorkingWithString.getBlockOfNumbers(document);
+        WorkingWithString.getLetterReplacement(document);
+        WorkingWithString.getLettersInLowerCase(document);
+        WorkingWithString.getLettersInUpperCase(document);
+        WorkingWithString.getLetterSequence(document);
+        WorkingWithString.getStartDigitSequence(document);
+        WorkingWithString.getEndsDigitSequence(document);
+    }
+}
---
+++ src/task1/WorkingWithString.java
@@ -0,0 +1,41 @@
+package task1;
+public class WorkingWithString {
+    public static void getBlockOfNumbers(String str) {
+        String[] array = str.split("-");
+        System.out.println(String.join("-", array[0], array[2]));
+    }
+
+    public static void getLetterReplacement(String str) {
+        String[] array = str.split("-");
+        String result1 = str.replace(array[1], "***");
+        String result2 = result1.replace(array[3], "***");
+        System.out.println(result2);
+    }
+
+    public static void getLettersInLowerCase(String str) {
+        String[] array = str.split("[0-9-]+");
+        for (int i = 1; i < array.length; i++) {
+            System.out.print(array[i].toLowerCase());
+            if (array.length != i) {
+                System.out.print("/");
+            }
+        }
+        System.out.println();
+    }
+
+    public static void getLettersInUpperCase(String str) {
+        StringBuilder sb = new StringBuilder(str);
+        System.out.println("Letters:" + sb.substring(5, 8).toUpperCase() + "/" + sb.substring(14, 17).toUpperCase() +
+                "/" + sb.substring(19, 20).toUpperCase() + "/" + sb.substring(21, 22).toUpperCase());
+    }
+    public static void getLetterSequence(String str) {
+            System.out.println(str.contains("ABC") || str.contains("abc"));
+        }
+
+    public static void getStartDigitSequence(String str) {
+        System.out.println(str.startsWith("555"));
+    }
+    public static void getEndsDigitSequence(String str) {
+        System.out.println(str.endsWith("1a2b"));
+    }
+}
